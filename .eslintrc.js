e2eb311c2d244a152a07decf5f8137e51685aba6
blacktunes <1719513828@qq.com>
2020-11-10 19:45:53 +0800

    初次提交

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
---
+++ bin/btn.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+const program = require('commander')
+const chalk = require('chalk')
+
+function cleanArgs(cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
+
+program
+  .version(`voices-button-cli ${require('../package').version}`)
+  .usage('<command> [options]')
+
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan(`vue <command> --help`)} for detailed usage of given command.`)
+  console.log()
+})
+
+program
+  .command('create <voives-button-name>')
+  .description('创建一个新的语音按钮')
+  .action(name => {
+    require('../lib/create')(name, process.cwd())
+  })
+
+program
+  .command('voices')
+  .description('根据public/voices目录生成voices.json文件')
+  .option('-d, --dir', '根据文件夹自动分类')
+  .action(() => {
+    require('../lib/voices')(process.cwd())
+  })
+
+program.parse(process.argv)
